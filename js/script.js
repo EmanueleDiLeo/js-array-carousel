4b42730cfacc91540dd2c4e1ade44949ff04fd54
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-09-13 18:47:15 +0200

    [ADD] : miniImg js

--- js/script.js
+++ js/script.js
@@ -9,23 +9,31 @@ const images =[
 const arrowTop = document.querySelector(".arrow-top");
 const arrowBot = document.querySelector(".arrow-bot");
 const boxImg = document.querySelector(".box-img");
+const miniImg = document.querySelector(".mini-img")
 
 let indexImg = 0;
 
+let heightMini = 100 / images.length;
+
 for(let i = 0; i < images.length; i++){
   boxImg.innerHTML += `<img class="hide item" src="${images[i]}" alt="${i+1}">`;
-  console.log(images[i]);
+  miniImg.innerHTML += `<img class="item-mini" src="${images[i]}" alt="${i+1}">`;
 }
 
-const itemsImg =document.getElementsByClassName("item");
+const itemsImg = document.getElementsByClassName("item");
+const itemsMiniImg = document.getElementsByClassName("item-mini");
+
+for(let i = 0; i < images.length; i++){
+  itemsMiniImg[i].style.height = heightMini + "%";
+}
 
 itemsImg[indexImg].classList.remove("hide");
-console.log(itemsImg[indexImg]);
+itemsMiniImg[indexImg].classList.add("active");
 
 // EVENTO CLICK FRECCIA SU
 arrowTop.addEventListener("click", function(){
   itemsImg[indexImg].classList.add("hide");
-  console.log(itemsImg[indexImg]);
+  itemsMiniImg[indexImg].classList.remove("active");
 
   if(indexImg === 0){
     indexImg = images.length - 1;
@@ -37,7 +45,7 @@ arrowTop.addEventListener("click", function(){
   }
 
   itemsImg[indexImg].classList.remove("hide");
-
+  itemsMiniImg[indexImg].classList.add("active");
 
 });
 
@@ -45,7 +53,7 @@ arrowTop.addEventListener("click", function(){
 arrowBot.addEventListener("click", function(){
 
   itemsImg[indexImg].classList.add("hide");
-  console.log(itemsImg[indexImg]);
+  itemsMiniImg[indexImg].classList.remove("active");
 
    if(indexImg === (images.length - 1)){
      indexImg = 0;
@@ -58,6 +66,7 @@ arrowBot.addEventListener("click", function(){
 
 
   itemsImg[indexImg].classList.remove("hide");
+  itemsMiniImg[indexImg].classList.add("active");
 
 });
 
